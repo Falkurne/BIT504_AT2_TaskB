358dc94c7c0d5fecc3c925a11839517c0f873475
Falkurne <corbyn19@gmail.com>
2022-05-07 15:56:01 +1200

    Multiple Additions
    Added Timer to PongPanel
    Added update class tied to actionperformed
    added sprite class with all supporting code

--- Pong Example/src/PongPanel.java
+++ Pong Example/src/PongPanel.java
@@ -1,18 +1,43 @@
-import java.awt.Color;
+	import java.awt.Color;
    import java.awt.event.ActionEvent;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
    import javax.swing.JPanel;
-   
+   import javax.swing.Timer;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
+   import java.awt.Stroke;
+   import java.awt.BasicStroke;
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
-	  private final static Color BACKGROUND_COLOUR = Color.BLACK;
-	   
-	  public PongPanel() {
-	      setBackground(BACKGROUND_COLOUR);
+	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	   private final static int TIMER_DELAY = 5;
+	        
+	    public PongPanel() {
+	        setBackground(BACKGROUND_COLOUR);
+	        Timer timer = new Timer(TIMER_DELAY, this);
+	           timer.start();
+	   }
+	    private void update() {
+	          
+	    }
+	  
+	    private void paintDottedLine(Graphics g) {
+	        Graphics2D g2d = (Graphics2D) g.create();
+	           Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	           g2d.setStroke(dashed);
+	           g2d.setPaint(Color.WHITE);
+	           g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	           g2d.dispose();
+	   }
+	  
+	  @Override
+	  public void paintComponent(Graphics g) {
+	      super.paintComponent(g);
+	      paintDottedLine(g);
 	  }
-  
+	    
       @Override
       public void keyPressed(KeyEvent event) {
       }
@@ -27,6 +52,8 @@ import java.awt.Color;
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
+    	  repaint();
       }
  
  }
